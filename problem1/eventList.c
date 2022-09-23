8c7d1465a7e4bcb5628da37467999aeb8fdb0b70
Emilio Jose <emilio.jimenez@upb.edu.co>
2022-09-22 23:20:29 -0500

    mensaje

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -5,12 +5,14 @@
 
 EventList *CreateEventList(void)
 {
-
+    EventList *eventList = malloc(sizeof(EventList));
+    eventList->isEmpty = 1;
+    eventList->head = NULL;
+    eventList->last = NULL;
 }
 
 void DestroyEventList(EventList *this)
 {
-
 }
 
 Event *SearchEvent(EventList *this, char *name)
@@ -20,7 +22,18 @@ Event *SearchEvent(EventList *this, char *name)
 
 void AddEvent(EventList *this, Event *event)
 {
-
+    if (this->isEmpty = 1)
+    {
+        this->isEmpty = 0;
+        this->head = event;
+        this->last = event;
+        event->next = NULL;
+    }
+    else
+    {
+        this->last->next = event;
+        this->last = event;
+    }
 }
 
 void RemoveEvent(EventList *this, char *name)
@@ -29,5 +42,12 @@ void RemoveEvent(EventList *this, char *name)
 
 void ListEvents(EventList *this)
 {
-
+    if (this->isEmpty == 1)
+    {
+        printf(*empty\n *);
+    }
+    else
+    {
+        printf(* %d\n *.this->head->eventName);
+    }
 }
